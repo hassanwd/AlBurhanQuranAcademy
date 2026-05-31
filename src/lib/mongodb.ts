df47e140b7a74f0d8ee9c81f51a95786e4167b9c
hassanwd <97110412+hassanwd@users.noreply.github.com>
2026-06-01 00:10:21 +0500

    Update MongoDb

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) throw new Error("Please define MONGODB_URI in .env.local");
-
-// Cached connection for Next.js hot reload
 const cached = (global as any).mongoose ?? { conn: null, promise: null };
 (global as any).mongoose = cached;
 
 export async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) throw new Error("Please define MONGODB_URI in .env.local");
+
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
